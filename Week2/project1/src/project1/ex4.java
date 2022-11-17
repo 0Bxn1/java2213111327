5ffb640dff3833898c041fa5ab126c01db37b7d1
0Bxn0 <siriyakornsangsui@gmail.com>
2022-11-17 15:55:39 +0700

    Exercise[11-17-2022]

---
+++ Week2/project1/src/project1/ex4.java
@@ -0,0 +1,29 @@
+package project1;
+import java.util.*;
+import java.text.*;
+import javax.swing.*;
+
+public class ex4 {
+
+	public static void main(String[] args) {
+		
+		String cdId="";
+		String cdTitle="";
+		int cdQuantity;
+		double cdPrice;
+		double cdSubtotal;
+		double cdTotal;
+		final double TAX_RATE = .0625;
+		cdId = JOptionPane.showInputDialog("This progeam calculater the total cost od a CD order Please enter the ID of the CD");
+		cdTitle =JOptionPane.showInputDialog("Please entr the title of the CD");
+		cdPrice = Double.parseDouble(
+				JOptionPane.showInputDialog("Please enter the price of the CD in U.S. dollars"));
+		cdQuantity = Integer.parseInt(
+				JOptionPane.showInputDialog("Please enter the quantity to be purchased"));
+		cdSubtotal =cdPrice * cdQuantity;
+		cdTotal =cdSubtotal * (1 + TAX_RATE);
+		JOptionPane.showMessageDialog(null,"Summary of the transaction:\n" + "\nCD ID:"+cdId+"\nCD Title:"+cdTitle+"\nCD Unit Price:$"+cdPrice+"\nCD Quantity: "+cdQuantity+
+				"\n\nSubtotal:$"+cdSubtotal+"\nTax rate:"+TAX_RATE*100+"%"+"\nTotal:$"+cdTotal+"\n\nEnd of Program");
+	}
+
+}
