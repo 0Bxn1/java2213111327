723705527593c0b843d17dd2b36b7b151f5d200d
0Bxn0 <siriyakornsangsui@gmail.com>
2022-12-22 12:57:07 +0700

    Attendance[22-12-2022]

--- Week3/ex1/src/ex1/Lab606.java
+++ Week3/ex1/src/ex1/Lab606.java
@@ -1,10 +1,14 @@
 package ex1;
+import javax.swing.*;
 
 public class Lab606 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = {25,78,41,22,36,85,37};
+		String index = JOptionPane.showInputDialog("Input index of array:");
+		
+		
+		
 	}
 
 }
