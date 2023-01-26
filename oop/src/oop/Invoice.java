f06903b22c150fc1ebbb43dfe40824006b3fa99e
weerawat phaphukdee <2213111160@TNISTUDENT.AC.TH>
2023-01-26 16:32:18 +0700

    practice2

--- oop/src/oop/Invoice.java
+++ oop/src/oop/Invoice.java
@@ -1,5 +1,36 @@
 package oop;
 
 public class Invoice {
+	private int id;
+	private Customer customer;
+	private double amount;
+	
+	public Invoice(int id,Customer customer,double amount) {
+		this.id = id;
+		this.customer = customer;
+		this.amount = amount;
+	}
+	public int getID() {
+		return id;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer() {
+		this.id = 0;
+		this.customer = customer;
+		this.amount = 0;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.id = 0;
+		this.customer = null;
+		this.amount = amount;
+	}
+//	public int getCustomerID() {
+//		return id;
+//	}
 
 }
